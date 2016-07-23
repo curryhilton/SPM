706b72064bedac0c69314e45e0c775322e818dd9
curryhilton <curry.hilton@gmail.com>
2016-07-22 19:51:26 -0500

    update server.r

--- R/server.r
+++ R/server.r
@@ -124,7 +124,7 @@ shinyServer(
         sub <- paste("Subgroups =", length(m))
         stdev <- paste("Standard Deviation =", round(sd))
         count <- paste("Violations =", length(which(x > uclx | x < lclx)))
-        arl <- paste("ARL =", round(1/((pnorm(uclx, cl, sd/sqrt(n), lower.tail = F)) + pnorm(lclx, cl, sd/sqrt(n))), 2))
+        arl <- paste("ARL =", round(1/((pnorm(uclx, cl, sd, lower.tail = F)) + pnorm(lclx, cl, sd)), 2))
 
         par(bg="lightsteelblue2", mar = c(10, 5, 2, 2))
         plot(m, x, xlab = "Subgroups", ylab = "X-bar", pch = 7, type = "b", ylim = c(lclx - rbar/2, uclx + rbar/2))
@@ -153,11 +153,32 @@ shinyServer(
         cl <- mean(x)                               # calculate x-bar baed on mean of subgroups
         uclx <- cl + (L*sbar)/(c4*sqrt(n))          # calculate upper control chart limit for x-bar chart
         lclx <- cl - (L*sbar)/(c4*sqrt(n))          # calculate lower control chart limit for x-bar chart
+        sd <- sbar/c4                               # calculate process sd
 
-        plot(m, x, xlab = "Subgroups", ylab = "X-bar", pch = 7, type = "b", ylim = c(lclx - sbar, uclx + sbar))
+        size <- paste("Subgroup Size =", n)
+        LCL <- paste("LCL =", round(lclx, 2))
+        CL <- paste("Center Line", round(cl, 2))
+        UCL <- paste("UCL =", round(uclx, 2))
+        sub <- paste("Subgroups =", length(m))
+        stdev <- paste("Standard Deviation =", round(sd))
+        count <- paste("Violations =", length(which(x > uclx | x < lclx)))
+        arl <- paste("ARL =", round(1/((pnorm(uclx, cl, sd, lower.tail = F)) + pnorm(lclx, cl, sd)), 2))
+
+        par(bg="lightsteelblue2", mar = c(10, 5, 2, 2))
+        plot(m, x, xlab = "Subgroups", ylab = "X-bar", pch = 7, type = "b", ylim = c(lclx - sbar/2, uclx + sbar/2))
+        rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = "white")
         abline(h = cl, lwd = 2)
         abline(h = uclx, col = "red", lty = 2)
         abline(h = lclx, col = "red", lty = 2)
+        points(x, pch = 20, type = "b", col = ifelse(x > uclx | x < lclx, "red", "black"))
+        mtext(size, at = m[1] + 1, side = 1, line = 5, adj = 0, font = 2)
+        mtext(LCL, at = m[1] + 1, side = 1, line = 6, adj = 0, font = 2)
+        mtext(CL, at = m[1] + 1, side = 1, line = 7, adj = 0, font = 2)
+        mtext(UCL, at = m[1] + 1, side = 1, line = 8, adj = 0, font = 2)
+        mtext(stdev, at = m[length(m)] - 1, side = 1, line = 5, adj = 1, font = 2)
+        mtext(sub, at = m[length(m)] - 1, side = 1, line = 6, adj = 1, font = 2)
+        mtext(count, at = m[length(m)] - 1, side = 1, line = 7, adj = 1, font = 2)
+        mtext(arl, at = m[length(m)] - 1, side = 1, line = 8, adj = 1, font = 2)
 
       } else if (input$chart == "R Chart, Standards Given") {
         sd <- as.numeric(input$sd)
@@ -179,15 +200,80 @@ shinyServer(
         D2 = d2[n-1] + L*d3[n-1]        # calculate control chart constant D2
         cl <- d2[n-1]*sd                # calculate centerline of R-bar chart
         uclr <- D2*sd                   # calculate upper control chart limit for R-bar chart
-        lclr <- D1*sd                   # calculate lower control chart limit for R-bar chart
+        lclr <- max(D1*sd, 0)           # calculate lower control chart limit for R-bar chart
 
-        plot(m, r, xlab = "Subgroups", ylab = "X-bar", pch = 7, type = "b", ylim = c(lclr - sd, uclr + sd))
+        size <- paste("Subgroup Size =", n)
+        LCL <- paste("LCL =", round(lclr, 2))
+        CL <- paste("Center Line", round(cl, 2))
+        UCL <- paste("UCL =", round(uclr, 2))
+        sub <- paste("Subgroups =", length(m))
+        stdev <- paste("Standard Deviation =", round(sd))
+        count <- paste("Violations =", length(which(r > uclr | r < lclr)))
+        arl <- paste("ARL =", round(1/((pnorm(uclr, cl, sd, lower.tail = F)) + pnorm(lclr, cl, sd)), 2))
+
+        par(bg="lightsteelblue2", mar = c(10, 5, 2, 2))
+        plot(m, r, xlab = "Subgroups", ylab = "X-bar", pch = 7, type = "b", ylim = c(lclr - sd/2, uclr + sd/2))
+        rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = "white")
         abline(h = cl, lwd = 2)
         abline(h = uclr, col = "red", lty = 2)
         abline(h = lclr, col = "red", lty = 2)
+        points(r, pch = 20, type = "b", col = ifelse(r > uclr | r < lclr, "red", "black"))
+        mtext(size, at = m[1] + 1, side = 1, line = 5, adj = 0, font = 2)
+        mtext(LCL, at = m[1] + 1, side = 1, line = 6, adj = 0, font = 2)
+        mtext(CL, at = m[1] + 1, side = 1, line = 7, adj = 0, font = 2)
+        mtext(UCL, at = m[1] + 1, side = 1, line = 8, adj = 0, font = 2)
+        mtext(stdev, at = m[length(m)] - 1, side = 1, line = 5, adj = 1, font = 2)
+        mtext(sub, at = m[length(m)] - 1, side = 1, line = 6, adj = 1, font = 2)
+        mtext(count, at = m[length(m)] - 1, side = 1, line = 7, adj = 1, font = 2)
+        mtext(arl, at = m[length(m)] - 1, side = 1, line = 8, adj = 1, font = 2)
 
       } else if (input$chart == "R Chart, No Standards Given") {
-        #plot()
+        L <- as.numeric(input$l)
+        n <- as.numeric(input$n)
+        m <- seq(1:dfm())
+        r <- dfx()
+        d2 <- c(1.128, 1.693, 2.059, 2.326, 2.534,  # define d2 control chart parameters
+                2.704, 2.847, 2.970, 3.078, 3.173,  # Montgomery's textbook
+                3.258, 3.336, 3.407, 3.472, 3.532,
+                3.588, 3.640, 3.689, 3.735, 3.778,
+                3.819, 3.858, 3.895, 3.931)
+        d3 <- c(0.853, 0.888, 0.880, 0.864, 0.848,  # define d3 control chart parameters
+                0.833, 0.820, 0.808, 0.797, 0.787,  # Montgomery's textbook
+                0.778, 0.770, 0.763, 0.756, 0.750,
+                0.744, 0.739, 0.734, 0.729, 0.724,
+                0.720, 0.716, 0.712, 0.708)
+        D3 = 1-L*(d3[n-1]/d2[n-1])                  # calculate control chart constant D3
+        D4 = 1+L*(d3[n-1]/d2[n-1])                  # calculate control chart constant D4
+        cl <- mean(r)                               # calculate centerline of R-bar chart
+        uclr <- D4*cl                               # calculate upper control chart limit for R-bar chart
+        lclr <- max(D3*cl, 0)                       # calculate lower control chart limit for R-bar chart
+        sd <- cl/d2[n-1]
+
+        size <- paste("Subgroup Size =", n)
+        LCL <- paste("LCL =", round(lclr, 2))
+        CL <- paste("Center Line", round(cl, 2))
+        UCL <- paste("UCL =", round(uclr, 2))
+        sub <- paste("Subgroups =", length(m))
+        stdev <- paste("Standard Deviation =", round(sd))
+        count <- paste("Violations =", length(which(r > uclr | r < lclr)))
+        arl <- paste("ARL =", round(1/((pnorm(uclr, cl, sd, lower.tail = F)) + pnorm(lclr, cl, sd)), 2))
+
+        par(bg="lightsteelblue2", mar = c(10, 5, 2, 2))
+        plot(m, r, xlab = "Subgroups", ylab = "X-bar", pch = 7, type = "b", ylim = c(lclr - sd/2, uclr + sd/2))
+        rect(par("usr")[1], par("usr")[3], par("usr")[2], par("usr")[4], col = "white")
+        abline(h = cl, lwd = 2)
+        abline(h = uclr, col = "red", lty = 2)
+        abline(h = lclr, col = "red", lty = 2)
+        points(r, pch = 20, type = "b", col = ifelse(r > uclr | r < lclr, "red", "black"))
+        mtext(size, at = m[1] + 1, side = 1, line = 5, adj = 0, font = 2)
+        mtext(LCL, at = m[1] + 1, side = 1, line = 6, adj = 0, font = 2)
+        mtext(CL, at = m[1] + 1, side = 1, line = 7, adj = 0, font = 2)
+        mtext(UCL, at = m[1] + 1, side = 1, line = 8, adj = 0, font = 2)
+        mtext(stdev, at = m[length(m)] - 1, side = 1, line = 5, adj = 1, font = 2)
+        mtext(sub, at = m[length(m)] - 1, side = 1, line = 6, adj = 1, font = 2)
+        mtext(count, at = m[length(m)] - 1, side = 1, line = 7, adj = 1, font = 2)
+        mtext(arl, at = m[length(m)] - 1, side = 1, line = 8, adj = 1, font = 2)
+
       } else if (input$chart == "s Chart, Standards Given") {
         #plot()
       } else if (input$chart == "s Chart, No Standards Given") {
